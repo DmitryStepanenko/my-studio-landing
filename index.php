a1632541c25f754fe5c518fe0d1add7cf47fbd08
Dmitry Stepanenko <w.kontakte@mail.ru>
2017-01-07 21:54:01 +0200

    first commit
    test
    add html def

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en/ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Landing studio</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	
+</body>
+
+<script src="js/main.js"></script>
+</html>
